279367ffdf6f9c9db3e48ba9ca68f536fd8564dd
fratzinger <22286818+fratzinger@users.noreply.github.com>
2022-01-05 14:28:10 +0100

    docs: add feathers links to header

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -14,6 +14,23 @@ export default {
     sidebar: [
       '/getting-started',
       '/hooks'
+    ],
+    navbar: [
+      {
+        text: 'Ecosystem',
+        children: [
+          { 
+            text: 'www.feathersjs.com', 
+            link: 'https://feathersjs.com/' 
+          }, {
+            text: "Feathers Github Repo",
+            link: "https://github.com/feathersjs/feathers"
+          }, {
+            text: 'Awesome Feathersjs',
+            link: 'https://github.com/feathersjs/awesome-feathersjs'
+          }
+        ]
+      }
     ]
   }
 }
