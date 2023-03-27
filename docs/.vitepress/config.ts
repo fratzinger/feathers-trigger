8d98788369ada995e2674afb466c6fa5e827f345
fratzinger <22286818+fratzinger@users.noreply.github.com>
2023-03-27 13:40:43 +0200

    docs: move to vitepress

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from "vitepress";
+
+export default {
+  title: 'feathers-trigger',
+  description: 'Add triggers and actions to your feathers app.',
+  head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
+  themeConfig: {
+    logo: '/img/logo.svg',
+    editLink: { pattern: 'https://github.com/fratzinger/feathers-trigger/edit/main/docs/:path', text: 'Edit this page on GitHub' },
+    lastUpdatedText: 'Last Updated',
+    socialLinks: [
+      {
+        icon: "twitter",
+        link: "https://twitter.com/feathersjs",
+      },
+      {
+        icon: "discord",
+        link: "https://discord.gg/qa8kez8QBx",
+      },
+      { icon: 'github', link: 'https://github.com/fratzinger/feathers-trigger' }
+    ],
+    nav: [
+      
+      {
+        text: 'Ecosystem',
+        items: [
+          { 
+            text: 'www.feathersjs.com', 
+            link: 'https://feathersjs.com/' 
+          }, {
+            text: "Feathers Github Repo",
+            link: "https://github.com/feathersjs/feathers"
+          }, {
+            text: 'Awesome Feathersjs',
+            link: 'https://github.com/feathersjs/awesome-feathersjs'
+          }
+        ]
+      }
+    ],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2021-present Frederik Schmatz'
+    },
+  }
+}
